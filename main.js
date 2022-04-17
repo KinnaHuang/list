70baf0d5a67d39ae8ab6d1bdd2149e049aa7e472
KinnaHuang <kt311038@gmail.com>
2022-04-17 21:27:34 +0800

    index.html

--- main.js
+++ main.js
@@ -1,49 +1,3 @@
-// var app = new Vue({
-//     el:'#app',
-//     data:{
-//     newAddText:'',
-//     newAddText2:'',
-//     newAddText3:'',
-        // itemList:[
-        //     {
-        //         id:'1',
-        //         itemName:'咖啡紅茶',
-        //         price:'20',
-        //         count:'1'
-        //     },
-        //     {
-        //         id:'2',
-        //         itemName:'茉香綠茶',
-        //         price:'30',
-        //         count:'4'
-        //     },
-        //     {
-        //         id:'3',
-        //         itemName:'珍珠奶茶',
-        //         price:'45',
-        //         count:'2'
-        //     },
-        // ],
-//         nextTodoId: 4
-//     },
-//     method:{
-//         addNewList: function(item){
-//             this.itemList.push({
-//                 id:this.nextTodoId++,
-//                 itemName:this.newAddText,
-//                 price:this.newAddText2,
-//                 count:this.newAddText3
-//             })
-//             this.newAddText=''
-//             this.newAddText2=''
-//             this.newAddText3=''
-//         }
-//     },	
-//     computed:{
-
-//     }
-// })
-
 var vm = new Vue({
     el: '#app',
     data: {
@@ -70,8 +24,8 @@ var vm = new Vue({
             count:'2'
         },
     ],
-        nextTodoId: 4
-    },
+    nextTodoId: 4
+},
     methods:{
         addNewList:function(){
             this.itemList.push({
@@ -86,3 +40,4 @@ var vm = new Vue({
         }
     }	
    })
+
